ab184db391ca2c7b948e4de1e7bcaf0ff0cb1144
Allan Amayo <my2allan@live.com>
2024-02-17 19:11:44 +0300

    Fixed the invoices, added pagnation

--- app/Http/Controllers/Api/v1/InvoiceController.php
+++ app/Http/Controllers/Api/v1/InvoiceController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\UpdateInvoiceRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\v1\InvoiceResource;
 use App\Http\Resources\v1\InvoiceCollection;
+use App\Filters\v1\InvoiceFilters;
+use Illuminate\Http\Request;
  
 
 class InvoiceController extends Controller
@@ -15,9 +17,20 @@ class InvoiceController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return new InvoiceCollection(Invoice::all());
+        $filter = new InvoiceFilters();
+        $queryItems = $filter->transform($request);
+
+        if(count($queryItems)==0){
+            return new InvoiceCollection(Invoice::paginate());
+        }else{
+            $invoices = Invoice::where($queryItems)->paginate();
+            return new InvoiceCollection($invoices->appends($request->query()));
+        }
+
+        
+        
     }
 
     /**
